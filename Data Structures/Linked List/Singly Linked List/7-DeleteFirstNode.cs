0a98f06bf11f5b841ea12e17d93c6b8613ff1149
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-10 23:36:18 +0530

    Create 7-DeleteFirstNode.cs

---
+++ Data Structures/Linked List/Singly Linked List/7-DeleteFirstNode.cs
@@ -0,0 +1,74 @@
+using System;
+
+//node structure
+class Node {
+  public int data;
+  public Node next;
+};
+
+class LinkedList {
+  Node head;
+
+  public LinkedList(){
+    head = null;
+  }
+  
+  //Add new element at the end of the list
+  public void push_back(int newElement) {
+    Node newNode = new Node();
+    newNode.data = newElement;
+    newNode.next = null; 
+    if(head == null) {
+      head = newNode;
+    } else {
+      Node temp = new Node();
+      temp = head;
+      while(temp.next != null)
+        temp = temp.next;
+      temp.next = newNode;
+    }    
+  }
+
+  //Delete first node of the list
+  public void pop_front() {
+    if(this.head != null) {
+      Node temp = this.head;
+      this.head = this.head.next;
+      temp = null;  
+    }
+  }
+
+  //display the content of the list
+  public void PrintList() {
+    Node temp = new Node();
+    temp = this.head;
+    if(temp != null) {
+      Console.Write("The list contains: ");
+      while(temp != null) {
+        Console.Write(temp.data + " ");
+        temp = temp.next;
+      }
+      Console.WriteLine();
+    } else {
+      Console.WriteLine("The list is empty.");
+    }
+  }     
+};
+
+// test the code
+class Implementation { 
+  static void Main(string[] args) {
+    LinkedList MyList = new LinkedList();
+
+    //Add four elements in the list.
+    MyList.push_back(10);
+    MyList.push_back(20);
+    MyList.push_back(30);
+    MyList.push_back(40);
+    MyList.PrintList();   
+
+    //Delete the first node
+    MyList.pop_front();
+    MyList.PrintList();  
+  }
+}
