4a3ae6b145e3c4699370a32f7fa3bc8337b13744
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-10 23:27:12 +0530

    Create NodeStructure.cs

---
+++ Data Structures/Linked List/Singly Linked List/NodeStructure.cs
@@ -0,0 +1,13 @@
+//node structure
+class Node {
+  public int data;
+  public Node next;
+};
+
+class LinkedList {
+  public Node head;
+  //constructor to create an empty LinkedList
+  public LinkedList(){
+    head = null;
+  }  
+};
