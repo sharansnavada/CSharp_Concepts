0d7f90f4ff338ea490ce141344eaf508b21e1218
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-10 23:42:51 +0530

    Create 96-DeleteFirstNodeByKey.cs

---
+++ Data Structures/Linked List/Singly Linked List/96-DeleteFirstNodeByKey.cs
@@ -0,0 +1,88 @@
+using System;
+
+//node structure
+class Node {
+  public int data;
+  public Node next;
+};
+
+class LinkedList {
+  Node head;
+
+  public LinkedList(){
+    head = null;
+  }
+  
+  //Add new element at the end of the list
+  public void push_back(int newElement) {
+    Node newNode = new Node(); 
+    newNode.data = newElement;
+    newNode.next = null;
+    if(head == null) {
+      head = newNode;
+    } else {
+      Node temp = new Node();
+      temp = head;
+      while(temp.next != null)
+        temp = temp.next;
+      temp.next = newNode;
+    }    
+  }
+
+  //Delete first node by key
+  public void pop_first(int key) {     
+    Node temp = head;
+    if(temp != null) {
+      if(temp.data == key) {
+        Node nodeToDelete = head;
+        head = head.next;
+        nodeToDelete = null;
+      } else {
+        while(temp.next != null) {
+          if(temp.next.data == key) {
+            Node nodeToDelete = temp.next;
+            temp.next = temp.next.next;
+            nodeToDelete = null;
+            break; 
+          }
+          temp = temp.next;
+        }
+      }
+    }
+  } 
+
+  //display the content of the list
+  public void PrintList() {
+    Node temp = new Node();
+    temp = this.head;
+    if(temp != null) {
+      Console.Write("The list contains: ");
+      while(temp != null) {
+        Console.Write(temp.data + " ");
+        temp = temp.next;
+      }
+      Console.WriteLine();
+    } else {
+      Console.WriteLine("The list is empty.");
+    }
+  }     
+};
+
+// test the code
+class Implementation {
+  static void Main(string[] args) {
+    LinkedList MyList = new LinkedList();
+
+    //Add five elements at the end of the list.
+    MyList.push_back(10);
+    MyList.push_back(20);
+    MyList.push_back(30);
+    MyList.push_back(10);
+    MyList.push_back(20);
+    MyList.PrintList(); 
+
+    //Delete first occurrence of 20
+    MyList.pop_first(20);
+    MyList.PrintList();     
+  }
+}
