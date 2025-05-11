944f7ea306538e4ce332ee7b5e21283a2587a496
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-12 01:34:04 +0530

    Create 4-CountNodes.cs

---
+++ Data Structures/Linked List/Doubly Linked List/4-CountNodes.cs
@@ -0,0 +1,81 @@
+using System;
+
+//node structure
+class Node {
+  public int data;
+  public Node next;
+  public Node prev;
+};
+
+class LinkedList {
+  Node head;
+
+  public LinkedList(){
+    head = null;
+  }
+  
+  //Add new element at the end of the list
+  public void push_back(int newElement) {
+    Node newNode = new Node();
+    newNode.data = newElement;
+    newNode.next = null; 
+    newNode.prev = null;
+    if(head == null) {
+      head = newNode;
+    } else {
+      Node temp = new Node();
+      temp = head;
+      while(temp.next != null)
+        temp = temp.next;
+      temp.next = newNode;
+      newNode.prev = temp;
+    }    
+  }
+
+  //count nodes in the list
+  public int countNodes() {
+    Node temp = new Node();
+    temp = this.head;
+    int i = 0;
+    while(temp != null) {
+      i++;
+      temp = temp.next;
+    }
+   return i;  
+  }  
+
+  //display the content of the list
+  public void PrintList() {
+    Node temp = new Node();
+    temp = this.head;
+    if(temp != null) {
+      Console.Write("The list contains: ");
+      while(temp != null) {
+        Console.Write(temp.data + " ");
+        temp = temp.next;
+      }
+      Console.WriteLine();
+    } else {
+      Console.WriteLine("The list is empty.");
+    }
+  }     
+};
+
+// test the code
+class Implementation {  
+  static void Main(string[] args) {
+    LinkedList MyList = new LinkedList();
+
+    //Add four elements in the list.
+    MyList.push_back(10);
+    MyList.push_back(20);
+    MyList.push_back(30);
+    MyList.push_back(40);
+
+    //Display the content of the list.
+    MyList.PrintList();  
+
+    //number of nodes in the list
+    Console.WriteLine("No. of nodes: "+ MyList.countNodes());   
+  }
+}
