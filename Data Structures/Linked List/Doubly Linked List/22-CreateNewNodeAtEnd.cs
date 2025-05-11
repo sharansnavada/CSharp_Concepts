5c35d996315e4f796a17f62def81931b484acdaf
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-12 01:28:50 +0530

    Create 22-CreateNewNodeAtEnd.cs

---
+++ Data Structures/Linked List/Doubly Linked List/22-CreateNewNodeAtEnd.cs
@@ -0,0 +1,63 @@
+using System;
+
+//node structure
+class Node {
+  public int data;
+  public Node next;
+  public Node prev;
+};
+
+class LinkedList {
+  Node head;
+
+  public LinkedList(){
+    head = null;
+  }
+  
+  //Add new element at the end of the list
+  public void push_back(int newElement) {
+    Node newNode = new Node();
+    newNode.data = newElement;
+    newNode.next = null; 
+    newNode.prev = null;
+    if(head == null) {
+      head = newNode;
+    } else {
+      Node temp = new Node();
+      temp = head;
+      while(temp.next != null)
+        temp = temp.next;
+      temp.next = newNode;
+      newNode.prev = temp;
+    }    
+  }
+
+  //display the content of the list
+  public void PrintList() {
+    Node temp = new Node();
+    temp = this.head;
+    if(temp != null) {
+      Console.Write("The list contains: ");
+      while(temp != null) {
+        Console.Write(temp.data + " ");
+        temp = temp.next;
+      }
+      Console.WriteLine();
+    } else {
+      Console.WriteLine("The list is empty.");
+    }
+  }     
+};
+
+// test the code
+class Implementation {  
+  static void Main(string[] args) {
+    LinkedList MyList = new LinkedList();
+
+    //Add three elements at the end of the list.
+    MyList.push_back(10);
+    MyList.push_back(20);
+    MyList.push_back(30);
+    MyList.PrintList();   
+  }
+}
