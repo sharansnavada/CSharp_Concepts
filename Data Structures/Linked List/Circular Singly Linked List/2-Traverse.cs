4eb7fe1113eec4906944f79c82923aca5c468d80
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-20 11:02:24 +0530

    Create 2-Traverse.cs

---
+++ Data Structures/Linked List/Circular Singly Linked List/2-Traverse.cs
@@ -0,0 +1,71 @@
+
+// Click on Run Code button to see the result
+
+using System;
+
+//node structure
+class Node {
+  public int data;
+  public Node next;
+};
+
+class LinkedList {
+  public Node head;
+  //constructor to create an empty LinkedList
+  public LinkedList(){
+    head = null;
+  } 
+
+  //display the content of the list
+  public void PrintList() {
+    Node temp = new Node();
+    temp = this.head;
+    if(temp != null) {
+      Console.Write("The list contains: ");
+      while(true) {
+        Console.Write(temp.data + " ");
+        temp = temp.next;
+        if(temp == this.head)
+          break;        
+      }
+      Console.WriteLine();
+    } else {
+      Console.WriteLine("The list is empty.");
+    }
+  }      
+};
+
+// test the code 
+class Implementation { 
+  static void Main(string[] args) {
+    //create an empty LinkedList
+    LinkedList MyList = new LinkedList();
+
+    //Add first node.
+    Node first = new Node();
+    first.data = 10;
+    //linking with head node
+    MyList.head = first;
+    //linking next of the node with head
+    first.next = MyList.head; 
+
+    //Add second node.
+    Node second = new Node();
+    second.data = 20;
+    //linking with first node
+    first.next = second;
+    //linking next of the node with head
+    second.next = MyList.head; 
+
+    //Add third node.
+    Node third = new Node();
+    third.data = 30;
+    //linking with second node
+    second.next = third; 
+    //linking next of the node with head
+    third.next = MyList.head; 
+
+    //print the content of list
+    MyList.PrintList();     
+  }
+}
