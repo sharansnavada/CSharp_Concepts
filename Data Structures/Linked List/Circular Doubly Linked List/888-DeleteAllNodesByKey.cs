9ab10f4b1fa51b2d00a17c722058ea9d5327a7d1
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-23 01:22:52 +0530

    Create 888-DeleteAllNodesByKey.cs

---
+++ Data Structures/Linked List/Circular Doubly Linked List/888-DeleteAllNodesByKey.cs
@@ -0,0 +1,44 @@
+public void pop_all(int key) {     
+  Node nodeToDelete = new Node();
+  Node temp = new Node();
+  
+  //1. if the head is not null and value stored at
+  //   head is equal to key, keep on adjusting the
+  //   head as head next and deleting previous head
+  //   until new head becomes null or not equal to key
+  while(this.head != null && this.head.data == key) {
+
+    if(this.head.next == this.head) {
+      this.head = null;
+    } else {
+      nodeToDelete = this.head;
+      temp = this.head;
+      // traverse to the last element of
+      // the list to adjust links
+      while(temp.next != this.head) {
+        temp = temp.next;
+      } 
+      this.head = this.head.next;
+      temp.next = this.head;   
+      this.head.prev = temp;
+      nodeToDelete = null;
+    }
+  }
+
+  //2. create a temp node to traverse through the
+  //   list and delete nodes with value equal to 
+  //   key and adjust links accordingly
+  temp = this.head;        
+  if(temp != null) {
+    while(temp.next != this.head) {
+      if(temp.next.data == key) {
+        nodeToDelete = temp.next;
+        temp.next = temp.next.next;
+        temp.next.prev = temp;
+        nodeToDelete = null;
+      } else {
+        temp = temp.next;
+      }
+    }
+  }
+} 
