4412b54a584c8856e83255a8863a6c0bf60fea7f
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-23 01:16:17 +0530

    Create 33-insertNewNodeAtEnd.cs

---
+++ Data Structures/Linked List/Circular Doubly Linked List/33-insertNewNodeAtEnd.cs
@@ -0,0 +1,34 @@
+public void push_back(int newElement) {
+  
+  //1. allocate node
+  Node newNode = new Node();
+  
+  //2. assign data element
+  newNode.data = newElement;
+  
+  //3. assign null to the next and prev
+  //   of the new node
+  newNode.next = null;
+  newNode.prev = null; 
+  
+  //4. Check the list is empty or not,
+  //   if empty make the new node as head
+  if(head == null) {
+    head = newNode;
+    newNode.next = head;
+    newNode.prev = head;
+  } else {
+    
+    //5. Else, traverse to the last node
+    Node temp = new Node();
+    temp = head;
+    while(temp.next != head)
+      temp = temp.next;
+    
+    //6. Adjust the links
+    temp.next = newNode;
+    newNode.next = head;
+    newNode.prev = temp;
+    head.prev = newNode;
+  }    
+}
