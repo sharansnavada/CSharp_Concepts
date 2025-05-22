ec1838a89d71815175e725905389b78084b0cd49
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-23 01:23:53 +0530

    Create 99-ReverseList.cs

---
+++ Data Structures/Linked List/Circular Doubly Linked List/99-ReverseList.cs
@@ -0,0 +1,35 @@
+public void reverseList() {
+  //1. If head is not null create three nodes
+  //   prevNode - pointing to head,
+  //   tempNode - pointing to head,
+  //   curNode - pointing to next of head
+  if(this.head != null) {
+    Node prevNode = this.head;
+    Node tempNode = this.head;
+    Node curNode = this.head.next;
+    
+    //2. assign next and previous of prevNode 
+    //   as itself to make the first node as last 
+    //   node of the reversed list
+    prevNode.next = prevNode;
+    prevNode.prev = prevNode;
+    
+    while(curNode != this.head) {
+      //3. While the curNode is not head adjust links 
+      //   (unlink curNode and link it to the reversed list 
+      //   from front and modify curNode and prevNode) 
+      tempNode = curNode.next;
+
+      curNode.next = prevNode;
+      prevNode.prev = curNode;
+      this.head.next = curNode;
+      curNode.prev = this.head;
+
+      prevNode = curNode;
+      curNode = tempNode;
+    }
+
+    //4. Make prevNode (last node) as head
+    this.head = prevNode;
+  }
+}  
