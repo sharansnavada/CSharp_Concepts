c0549718f8dc4f0b307c1db073b42085b7e2136a
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-23 01:17:26 +0530

    Create 4-DeleteNodeAtFirst.cs

---
+++ Data Structures/Linked List/Circular Doubly Linked List/4-DeleteNodeAtFirst.cs
@@ -0,0 +1,29 @@
+public void pop_front() {
+  if(this.head != null) {
+    
+    //1. the list contains one node, delete
+    //   make the head null
+    if(this.head.next == this.head) {
+      this.head = null;
+    } else {
+      
+      //2. if the list contains more than one node,
+      //   create two nodes - temp and firstNode, both
+      //   pointing to head node
+      Node temp = this.head;
+      Node firstNode = this.head;
+      
+      //3. using temp node, traverse to the last node
+      while(temp.next != this.head) {
+        temp = temp.next;
+      }
+      
+      //4. Make head as next of head, and 
+      //   update links
+      this.head = this.head.next;
+      this.head.prev = temp;
+      temp.next = this.head; 
+      firstNode = null; 
+    }
+  }
+}
