b793caa181779e920572d37be0e807ed07872688
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-23 01:16:59 +0530

    Create 333-InsertNewNodeAtPosition.cs

---
+++ Data Structures/Linked List/Circular Doubly Linked List/333-InsertNewNodeAtPosition.cs
@@ -0,0 +1,55 @@
+public void push_at(int newElement, int position) { 
+  
+  //1. allocate node to new element and 
+  //   create a temp node to traverse the list 
+  Node newNode = new Node(); 
+  newNode.data = newElement;
+  newNode.next = null;
+  Node temp = head;
+  int NoOfElements = 0;
+
+  //2. Find the number of elements in the list
+  if(temp != null) {
+    NoOfElements++;
+    temp = temp.next;
+  }
+  while(temp != head) {
+    NoOfElements++;
+    temp = temp.next;
+  }
+
+  //3. check if the insertion position is valid
+  if(position < 1 || position > (NoOfElements+1)) {
+    Console.Write("\nInvalid position.");
+  } else if (position == 1) {
+  
+  //4. if the position is 1, make next of the
+  //   new node as head and new node as head
+    if(head == null) {
+      head = newNode;
+      head.next = head;
+      head.prev = head;
+    } else {
+      while(temp.next != head) {
+        temp = temp.next;
+      }
+      temp.next = newNode;
+      newNode.prev = temp;
+      newNode.next = head;
+      head.prev = newNode;
+      head = newNode;
+    }
+  } else {
+
+   //5. Else, traverse to the node previous to
+   //   the given position, make newNode next 
+   //   as temp next and temp next as newNode.
+    temp = head;
+    for(int i = 1; i < position-1; i++) 
+      temp = temp.next;
+    newNode.next = temp.next;
+    newNode.next.prev = newNode;
+    newNode.prev = temp;
+    temp.next = newNode;  
+  }
+}
